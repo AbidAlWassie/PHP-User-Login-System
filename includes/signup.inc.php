dcfa0341a419d10402d4b174c1bf255a723aa182
Abid Al-Wasi <abidalwasi@outlook.com>
2021-04-21 23:29:59 +0600

    updated project

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -7,8 +7,16 @@ $em = $_POST['email'];
 $uid = $_POST['username'];
 $pwd = $_POST['password'];
 
-$sql = "INSERT INTO users (user_firstname, user_lastname, user_email, user_name, user_password) VALUES ('$fst','$lst','$em','$uid','$pwd');";
-mysqli_query($connect, $sql);
+$sql = "INSERT INTO `users` (user_firstname, user_lastname, user_email, user_name, user_password) VALUES (?, ?, ?, ?, ?);";
+// mysqli_query($connect, $sql);
+$stmt = mysqli_stmt_init($connect);
+
+if (!mysqli_stmt_prepare($stmt, $sql)) {
+  echo "Failed";
+} else {
+  mysqli_stmt_bind_param($stmt, "sssss", $fst, $lst, $em, $uid, $pwd);
+  mysqli_stmt_execute($stmt);
+}
 
 header("Location: ../index.php?signup=success");
 
