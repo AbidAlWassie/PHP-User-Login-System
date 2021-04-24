7601a0f8b5821dbfcc36992c4a22bb92cd3c1ffb
Abid Al-Wasi <abidalwasi@outlook.com>
2021-04-25 01:38:56 +0600

    updated project

--- index.php
+++ index.php
@@ -10,7 +10,15 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css/style.css">
-	
+	<style>
+		.navbar li a {
+			user-select: none;
+		}
+
+		ul.links {
+			user-select: none;
+		}
+	</style>
 <!-- <script src="js/script.js"></script> -->
 
 	<title>Home Page</title>
@@ -19,11 +27,11 @@
 
 <div class="navbar">
 	<ul>
-		<li><a class="active" href="#home">Home</a></li>
-		<li><a href="#news">News</a></li>
-		<li><a href="#contact">Contact</a></li>
-		<li><a href="#about">About</a></li>
-		<li><a href="#signin" id="loginBtn">Sign In</a></li>
+		<li><a draggable="false" class="active" href="#home">Home</a></li>
+		<li><a draggable="false" href="#news">News</a></li>
+		<li><a draggable="false" href="#contact">Contact</a></li>
+		<li><a draggable="false" href="#about">About</a></li>
+		<li><a draggable="false" href="#signin" id="loginBtn">Sign In</a></li>
 	</ul>
 </div>
 
@@ -32,9 +40,9 @@
 
     <div class="options-nav">
       <ul class="links">
-        <li class="one signin-active"><a class="signinBtn">Sign in</a></li>
-        <li class="two signup-inactive"><a class="signupBtn">Sign up</a></li>
-        <li class=""><span class="close">&times;</span></li>
+        <li draggable="false" class="one signin-active"><a class="signinBtn">Sign in</a></li>
+        <li draggable="false" class="two signup-inactive"><a class="signupBtn">Sign up</a></li>
+        <li draggable="false" class=""><span class="close">&times;</span></li>
       </ul>
     </div>
 
