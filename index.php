ed71ec9a6b1b081cb6e30ca44f5ad9145ba47cff
Abid Al-Wasi <abidalwasi@outlook.com>
2021-04-25 18:43:06 +0600

    updated project

--- index.php
+++ index.php
@@ -74,73 +74,73 @@
     </div>
   </div>
 </div>
+
 <script>
 		
-var modal = document.getElementById("modal-area");
-var modalContent = document.querySelector("modal-content");
+	var modal = document.getElementById("modal-area");
+	var modalContent = document.querySelector("modal-content");
 
-var btn = document.getElementById("loginBtn");
+	var btn = document.getElementById("loginBtn");
 
-var span = document.getElementsByClassName("close")[0];
+	var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-  modal.style.display = "flex";
-}
+	btn.onclick = function() {
+		modal.style.display = "block";
+	}
 
-span.onclick = function() {
-  modal.style.display = "none";
-}
+	span.onclick = function() {
+		modal.style.display = "none";
+	}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
 	</script>
 <script>
 	window.onload = () => {
 
-	let signinBtn = document.querySelector(".signinBtn");
-	let signupBtn = document.querySelector(".signupBtn");
+		let signinBtn = document.querySelector(".signinBtn");
+		let signupBtn = document.querySelector(".signupBtn");
 
-	signinBtn.addEventListener("click", signin);
-	signupBtn.addEventListener("click", signup);
+		signinBtn.addEventListener("click", signin);
+		signupBtn.addEventListener("click", signup);
 
-	const signupInactive = document.querySelector(".one");
-	const signinActive = document.querySelector(".two");
+		const signupInactive = document.querySelector(".one");
+		const signinActive = document.querySelector(".two");
 
-	const forgotPassword = document.querySelector(".forgot");
+		const forgotPassword = document.querySelector(".forgot");
 
-	const formSignIn = document.querySelector(".form-signin");
-	const formSignUp = document.querySelector(".form-signup");
+		const formSignIn = document.querySelector(".form-signin");
+		const formSignUp = document.querySelector(".form-signup");
 
-	const frame = document.querySelector(".frame");
+		const frame = document.querySelector(".frame");
 
-	function signin() {
-		forgotPassword.classList.remove("forgot-left");
-		frame.classList.remove("frame-long");
-		formSignIn.classList.remove("form-signin-left");
-		formSignUp.classList.remove("form-signup-left");
-		signinActive.classList.remove("signup-active");
-		signinActive.classList.add("signin-inactive");
-		signupInactive.classList.remove("signin-inactive");
-		// console.log("Sign In");
+		function signin() {
+			forgotPassword.classList.remove("forgot-left");
+			frame.classList.remove("frame-long");
+			formSignIn.classList.remove("form-signin-left");
+			formSignUp.classList.remove("form-signup-left");
+			signinActive.classList.remove("signup-active");
+			signinActive.classList.add("signin-inactive");
+			signupInactive.classList.remove("signin-inactive");
+			// console.log("Sign In");
+		}
+
+		function signup() {
+			forgotPassword.classList.add("forgot-left");
+			frame.classList.add("frame-long");
+			formSignIn.classList.add("form-signin-left");
+			formSignUp.classList.add("form-signup-left");
+			signinActive.classList.add("signup-active");
+			signinActive.classList.remove("signup-inactive");
+			signupInactive.classList.add("signin-inactive");
+			// console.log("Sign Up");
+		}
 	}
-
-	function signup() {
-		forgotPassword.classList.add("forgot-left");
-		frame.classList.add("frame-long");
-		formSignIn.classList.add("form-signin-left");
-		formSignUp.classList.add("form-signup-left");
-		signinActive.classList.add("signup-active");
-		signinActive.classList.remove("signup-inactive");
-		signupInactive.classList.add("signin-inactive");
-		// console.log("Sign Up");
-	}
-}
-
-</script>
+	</script>
 <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script> -->
 </body>
 </html>
@@ -149,7 +149,7 @@ window.onclick = function(event) {
 	$data = "Admin";
 	$sql = "SELECT * FROM `users` WHERE `user_name`=?;";
 	// $result = mysqli_query($connect, $sql);
-	// $resultCheck = mysqli_num_rows($result);
+	// $resultCheck = mysqli_num_rows($result); 
 
 	$stmt = mysqli_stmt_init($connect);
 	if (!mysqli_stmt_prepare($stmt, $sql)) {
